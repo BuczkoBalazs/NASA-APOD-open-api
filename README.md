2a8da4f94539f17ce538bd2229162839bafbbf25
BuczkoBalazs <buci85@gmail.com>
2022-06-01 18:22:45 +0200

    Initial upload of files.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# NASA-APOD-open-api
+# Welcome to my APOD NASA free API repository!
+
+## Short summary
+
+This is a project where I use API to get data using Javascript.
+I used the APOD API which can be found [here](https://api.nasa.gov/).
+If you don't find it just search for APOD.
+
+## What the page knows
+
+Page shows the information of the actual day APOD shows.
+Basically an image with it's title and description.
+
+If you would like to search information of a different day there is a possibility to add it into the header input field.
+
+If you want to check the page please click on the following [link](https://buczkobalazs.github.io/NASA-APOD-open-api/).
+
+## What needs to be done
+
+I have some plans how to improve the page in the upcoming weeks.
+The following list should be done:
+
+- Style it with css not to look so odd
+- Separate today's information from the section where you can choose your preferred date
+- Create a so-called 'Gallery' section where you can swipe through random images provided by the API
+
+
+## Thank you for visiting my repository!
---
+++ script.js
@@ -0,0 +1,91 @@
+// HTML ELEMENTS
+
+const header = () => {
+    return `
+    <form>
+        <label for="date">Write a date in format YYYY-MM-DD</label>
+        <input type="text" id="date" name="date">
+        <button class="submit-date">Show me</button>
+    </form>
+    `
+}
+
+const todayImageSection = (url, title, explanation) => {
+    return `
+    <section id="todaySection">
+        <img src=${url}>
+        <p>Title: ${title}</p>
+        <article>Explanation: ${explanation}</article>
+    </section>
+    `
+};
+
+const todayVideoSection = (url, title, explanation) => {
+    return `
+    <section id="todaySection">
+        <iframe src=${url}></iframe>
+        <p>Title: ${title}</p>
+        <article>Explanation: ${explanation}</article>
+    </section>
+    `
+};
+
+const gallery = (url) => {
+    return `
+    <section id="gallery">
+        <image src=${url}>
+    </section>
+    `
+}
+
+// FUNCTIONS
+
+// FETCH & RENDER
+const getData = async (url, apiKey, date) => {
+    const rootElement = document.getElementById("root");
+
+    fetch(`${url}${apiKey}&date=${date}`)
+    .then(res => res.json())
+    .then((data) => {
+        return (data.media_type === "image") ? rootElement.insertAdjacentHTML("beforeend", todayImageSection(data.url, data.title, data.explanation)) : rootElement.insertAdjacentHTML("beforeend", todayVideoSection(data.url, data.title, data.explanation))
+    })
+    .catch( err => console.log(err));
+};
+
+// LOADEVENT
+
+const loadEvent = async () => {
+
+    // VARIABLES
+    const rootElement = document.getElementById("root");
+    const url = "https://api.nasa.gov/planetary/apod";
+    const apiKey = "?api_key=7ju7WWOshTgaMnMDVMQCe1DITfBsMJfO5rXe61hn";
+    let today = new Date().toISOString().slice(0, 10);
+    
+    // RENDER HEADER
+    rootElement.insertAdjacentHTML("beforeend", header());
+
+    // FETCH & RENDER TODAY'S INFO
+    getData(url, apiKey, today)
+
+    //FETCH & RENDER INFO GIVEN BY USER
+
+    const getUserGivenDate = (e) => {
+        e.preventDefault();
+        let classList = e.target.classList;
+        if (classList.contains("submit-date")) {
+            if(document.getElementById("todaySection")){
+                document.getElementById("todaySection").remove();
+            }
+
+            let date = document.getElementById("date").value
+     
+            getData(url, apiKey, date)
+        }
+    };
+    
+    // ADDEVENT LISTENERS
+    document.addEventListener("click", getUserGivenDate);
+    
+};
+window.addEventListener("load", loadEvent);
